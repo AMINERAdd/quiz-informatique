cf47f6d39d1015087ad73aa6110c6da1b5d84ab9
AMINERAdd <146769000+AMINERAdd@users.noreply.github.com>
2026-04-28 20:31:20 +0100

    Add Firebase configuration to firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,14 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyAF7aaeksLKsjCBLhDzTDpJjfv8fapq8WM",
+    authDomain: "excel-e3987.firebaseapp.com",
+    projectId: "excel-e3987",
+    storageBucket: "excel-e3987.firebasestorage.app",
+    messagingSenderId: "916795973569",
+    appId: "1:916795973569:web:4b51c831384f9d9c845940",
+    measurementId: "G-Z8B1GNY2QK"
+};
+const appId = "plickers-1apic-radouani-live";
+// Initialize Firebase
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
